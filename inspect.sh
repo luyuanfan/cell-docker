269e7f02b28e5c87a661bc74c4462d2650bb9aaa
Jasmine Fan <lyspfan@gmail.com>
2025-10-23 18:16:19 -0400

    now inspect is actually updated :'p

--- inspect.sh
+++ inspect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONTAINER_NAME="luyuanfan/enb"
+CONTAINER_NAME="luyuanfan/gnb"
 CONTAINER_ID=$(docker ps | grep -e "$CONTAINER_NAME" | awk '{print $1}')
 
 docker exec -it "$CONTAINER_ID" bash
