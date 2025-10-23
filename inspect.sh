5c351cccd1c99838de57cc19081249d5fc1fb87a
Jasmine Fan <lyspfan@gmail.com>
2025-10-23 18:14:26 -0400

    updated image name and tag to gnb:x310 in inspect scripts as well

--- inspect.sh
+++ inspect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CONTAINER_NAME="luyuanfan/enb"
-CONTAINER_ID=$(docker ps | grep "$CONTAINER_NAME" | awk '{print $1}')
+CONTAINER_ID=$(docker ps | grep -e "$CONTAINER_NAME" | awk '{print $1}')
 
 docker exec -it "$CONTAINER_ID" bash
