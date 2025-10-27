7ff30bf69b295d6b2189da6ff4e12d8cc578eda5
Jasmine Fan <lyspfan@gmail.com>
2025-10-27 15:19:26 -0400

    move mtu tune to initoperator

--- operator.sh
+++ operator.sh
@@ -6,10 +6,5 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-# ./docker/scripts/srsran_performance # Contains sysctl commands, cannot do in containers
-ifconfig enp12s0f1np1 mtu 9000 # For 10 GigE
-# chmod 700 /docker/scripts/srsran_performance
-# ./docker/scripts/srsran_performance
-
-docker run -ti --privileged --network host --rm -v /dev/:/dev/ -e CONFIG64="$(base64 $1)" luyuanfan/gnb:x310 ./initOperator.sh
+docker run -ti --privileged --network host --rm --ulimit rtprio=99 --ulimit memlock=-1 -v /dev/:/dev/ -e CONFIG64="$(base64 $1)" luyuanfan/gnb:x310 ./initOperator.sh
 # docker run -ti --privileged --rm -v /dev/:/dev/ -e CONFIG64="$(base64 $1)" luyuanfan/gnb:x310 ./initOperator.sh
