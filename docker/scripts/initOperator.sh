5368343fa509b4f02f06529d697008f80e56996b
Jasmine Fan <lyspfan@gmail.com>
2025-10-27 15:15:31 -0400

    change ogstun back; device busy was because container runs on host mode and changes it makes persist across sessions

--- docker/scripts/initOperator.sh
+++ docker/scripts/initOperator.sh
@@ -42,14 +42,14 @@ fi
 ##########
 
 # Create TUN device
-ip tuntap add name ogstun0 mode tun
-ip addr add 10.45.0.1/16 dev ogstun0
-ip addr add 2001:db8:cafe::1/48 dev ogstun0
-ip link set ogstun0 up
+ip tuntap add name ogstun mode tun
+ip addr add 10.45.0.1/16 dev ogstun
+ip addr add 2001:db8:cafe::1/48 dev ogstun
+ip link set ogstun up
 
 # Connect core to the internet
 sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
 
 # Wait until mongo DB gets initialized
 while true;
