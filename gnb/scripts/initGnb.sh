5b5d125b6fbf03125471e9ffb321eb9479530ac5
Jasmine Fan <lyspfan@gmail.com>
2025-12-04 15:08:54 -0500

    du b205 config correction

--- gnb/scripts/initGnb.sh
+++ gnb/scripts/initGnb.sh
@@ -6,112 +6,22 @@
 
 echo "Etc/Universal" > /etc/timezone
 
-# #############
-# #  MongoDB  #
-# #############
-
-# # if mongo is not running, execute it in the background
-# mkdir -p /data/db
-# chown -R mongodb:mongodb /data/db || true
-
-# if ! nc -z localhost 27017; then
-#     echo "Starting MongoDB manually..."
-# 	mongod --fork --logpath /mongod.log
-# fi
-
-##########
-#  Core  #
-##########
-
-# wait until mongo DB gets initialized
-# while true;
-# do
-# 	nc -zvv localhost 27017 > /dev/null 2>&1
-# 	if [ $? -eq 0 ]; then
-# 		break
-# 	else
-# 		echo "Waiting for MongoDB..."
-# 		sleep 1
-# 	fi
-# done
-
-# # populate core database
-# add_ue_with_apn {imsi key opc apn}
-# type {imsi type}: changes the PDN-Type of the first PDN: 1 = IPv4, 2 = IPv6, 3 = IPv4v6"
-# for i in $(seq -f "%010g" 1 $NUM_UES)
-# do
-# 	/open5gs/misc/db/open5gs-dbctl reset
-# 	/open5gs/misc/db/open5gs-dbctl add_ue_with_apn $IMSI $KEY $OPC $APN
-# 	/open5gs/misc/db/open5gs-dbctl type $IMSI $TYPE
-# done
-
-# /open5gs/misc/db/open5gs-dbctl reset
-# /open5gs/misc/db/open5gs-dbctl add_ue_with_apn $IMSI $KEY $OPC $APN
-# /open5gs/misc/db/open5gs-dbctl type $IMSI $TYPE
-
-# sed -i "s/NETWORK_MCC/$MCC/g" amf.yaml
-# sed -i "s/NETWORK_MNC/$MNC/g" amf.yaml
-# sed -i "s/NETWORK_APN/$APN/g" amf.yaml
-# sed -i "s/NETWORK_MCC/$MCC/g" nrf.yaml
-# sed -i "s/NETWORK_MNC/$MNC/g" nrf.yaml
-# sed -i "s/NETWORK_APN/$APN/g" smf.yaml
-
-# echo "Running 5G SA Core Network"
-
-# /open5gs/install/bin/open5gs-nrfd -c /nrf.yaml &        # discover other core services
-# /open5gs/install/bin/open5gs-scpd &                     # enable indirect communication           
-# # /open5gs/install/bin/open5gs-seppd &                  # roaming security
-# /open5gs/install/bin/open5gs-amfd -c /amf.yaml &        # subscriber authentication
-# /open5gs/install/bin/open5gs-smfd -c /smf.yaml &        # session management
-# /open5gs/install/bin/open5gs-upfd -c /upf.yaml &        # transport data packets between gnb and external WAN
-# /open5gs/install/bin/open5gs-ausfd &                    # next three do sim authentication and hold user profile
-# /open5gs/install/bin/open5gs-udmd & 
-# /open5gs/install/bin/open5gs-udrd &
-# /open5gs/install/bin/open5gs-pcfd &                     # charging & enforcing subscriber policies
-# /open5gs/install/bin/open5gs-nssfd &                    # allow selecting network slice
-# /open5gs/install/bin/open5gs-bsfd &                     # binding support function
-# /open5gs/install/bin/open5gs-mmed &                     # below are all LTE services
-# /open5gs/install/bin/open5gs-sgwcd & 
-# /open5gs/install/bin/open5gs-sgwud & 
-# /open5gs/install/bin/open5gs-hssd & 
-# /open5gs/install/bin/open5gs-pcrfd &
-
-# sleep 1
-
-############
-#  RAN 5G  #
-############
-
-# sed -i "s/NETWORK_MCC/$MCC/g" gnb.yml
-# sed -i "s/NETWORK_MNC/$MNC/g" gnb.yml
-# sed -i "s/USRP_ID/$USRP/g" gnb.yml
-# if [[ ${MIMO,,} == "yes" ]]; then 
-# 	TRANSMISSION_MODE=4
-# 	NUM_PORTS=2
-# else
-# 	TRANSMISSION_MODE=1
-# 	NUM_PORTS=1
-# fi
-# sed -i "s/TRANSMISSION_MODE/$TRANSMISSION_MODE/g" gnb.yml
-# sed -i "s/NUM_PORTS/$NUM_PORTS/g" gnb.yml
-
-# chrt --rr 99 gnb -c gnb.yml
-
-
 ## CU
 
 sed -i "s/NETWORK_MCC/$MCC/g" cu.yml
 sed -i "s/NETWORK_MNC/$MNC/g" cu.yml
 sed -i "s/USRP_ID/$USRP/g" cu.yml
 
-## DU
+## DU B200
 
 sed -i "s/NETWORK_MCC/$MCC/g" du_b200.yml
 sed -i "s/NETWORK_MNC/$MNC/g" du_b200.yml
-sed -i "s/USRP_ID/$USRP/g" du_b200.yml
-# sed -i "s/NETWORK_MCC/$MCC/g" du_b205.yml
-# sed -i "s/NETWORK_MNC/$MNC/g" du_b205.yml
-# sed -i "s/USRP_ID/$USRP/g" du_b205.yml
+sed -i "s/USRP_ID/$USRP1/g" du_b200.yml
+
+## DU B205
+sed -i "s/NETWORK_MCC/$MCC/g" du_b205.yml
+sed -i "s/NETWORK_MNC/$MNC/g" du_b205.yml
+sed -i "s/USRP_ID/$USRP2/g" du_b205.yml
 
 (
 	cd /srsRAN_Project/build/apps/cu && chrt --rr 99 srscu -c /cu.yml &
