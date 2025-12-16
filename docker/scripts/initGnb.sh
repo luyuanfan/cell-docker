d86862eb06433ac7ec5487aba5d17014450a7271
Jasmine Fan <lyspfan@gmail.com>
2025-12-16 12:37:48 -0500

    separate services

--- docker/scripts/initGnb.sh
+++ docker/scripts/initGnb.sh
@@ -7,31 +7,23 @@
 echo "Etc/Universal" > /etc/timezone
 
 ## CU
-
-sed -i "s/NETWORK_MCC/$MCC/g" cu.yml
-sed -i "s/NETWORK_MNC/$MNC/g" cu.yml
-sed -i "s/USRP_ID/$USRP1/g" cu.yml
-
+if [[ "$CONTAINER_NAME" == "cu" ]]; then
+    sed -i "s/NETWORK_MCC/$MCC/g" cu.yml
+    sed -i "s/NETWORK_MNC/$MNC/g" cu.yml
+    sed -i "s/USRP_ID/$USRP1/g" cu.yml
+    cd /srsRAN_Project/build/apps/cu && chrt --rr 99 srscu -c /cu.yml
+fi
 ## DU B200
-
-sed -i "s/NETWORK_MCC/$MCC/g" du_b200.yml
-sed -i "s/NETWORK_MNC/$MNC/g" du_b200.yml
-sed -i "s/USRP_ID/$USRP1/g" du_b200.yml
-
+if [[ "$CONTAINER_NAME" == "du_1" ]]; then
+    sed -i "s/NETWORK_MCC/$MCC/g" du_b200.yml
+    sed -i "s/NETWORK_MNC/$MNC/g" du_b200.yml
+    sed -i "s/USRP_ID/$USRP1/g" du_b200.yml
+    cd /srsRAN_Project/build/apps/du && chrt --rr 99 srsdu -c /du_b200.yml
+fi 
 ## DU B205
-sed -i "s/NETWORK_MCC/$MCC/g" du_b205.yml
-sed -i "s/NETWORK_MNC/$MNC/g" du_b205.yml
-sed -i "s/USRP_ID/$USRP2/g" du_b205.yml
-
-# tmux new -s gnb 
-# tmux split-window -h 'cd /srsRAN_Project/build/apps/cu && chrt --rr 99 srscu -c /cu.yml'
-# tmux split-window -h 'cd /srsRAN_Project/build/apps/du && chrt --rr 99 srsdu -c /du_b200.yml'
-# tmux split-window -h 'cd /srsRAN_Project/build/apps/du && chrt --rr 99 srsdu -c /du_b205.yml'
-
-wait
-
-# (
-# 	cd /srsRAN_Project/build/apps/cu && chrt --rr 99 srscu -c /cu.yml &
-# 	cd /srsRAN_Project/build/apps/du && chrt --rr 99 srsdu -c /du_b200.yml &
-# 	cd /srsRAN_Project/build/apps/du && chrt --rr 99 srsdu -c /du_b205.yml &
-# ) 2>&1 | tee /gnb.log
+if [[ "$CONTAINER_NAME" == "du_2" ]]; then
+    sed -i "s/NETWORK_MCC/$MCC/g" du_b205.yml
+    sed -i "s/NETWORK_MNC/$MNC/g" du_b205.yml
+    sed -i "s/USRP_ID/$USRP2/g" du_b205.yml
+    cd /srsRAN_Project/build/apps/du && chrt --rr 99 srsdu -c /du_b205.yml
+fi 
