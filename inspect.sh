f6a88123a9d7f6a766177a580eb97cd540e4271e
Jasmine Fan <lyspfan@gmail.com>
2026-01-05 14:03:21 -0500

    control which container to inspect
    still have open5gs mounting issues i think it's how the source code is
    built

--- inspect.sh
+++ inspect.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-CONTAINER_NAME="core"
+if [ "$1" = "-c" ]; then
+    CONTAINER_NAME="core"
+else
+    CONTAINER_NAME="gnb"
+fi
+
 CONTAINER_ID=$(docker ps -qf name=$CONTAINER_NAME)
 
 docker exec -it "$CONTAINER_ID" bash
