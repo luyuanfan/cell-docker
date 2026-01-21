0f7fd3cbeda9abae95f1469b4525cb196b3a3862
Jasmine Fan <lyspfan@gmail.com>
2026-01-21 15:13:24 -0500

    separate attach from inspect script

---
+++ attach.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$1" = "-c" ]; then
+    CONTAINER_NAME="core"
+else
+    CONTAINER_NAME="gnb"
+fi
+
+CONTAINER_ID=$(docker ps -qf name=$CONTAINER_NAME)
+
+docker attach "$CONTAINER_ID"
