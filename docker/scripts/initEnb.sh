42adf46829d5d8d49df0e37bd77c07385915c69d
Jasmine Fan <lyspfan@gmail.com>
2026-03-31 17:44:01 -0400

    adjust logging time, use this version

--- docker/scripts/initEnb.sh
+++ docker/scripts/initEnb.sh
@@ -15,5 +15,6 @@ sed -i "s/NUM_PRBS/$NUM_PRBS/g" enb.conf
 sed -i "s/TRANSMISSION_MODE/$TRANSMISSION_MODE/g" enb.conf
 sed -i "s/NUM_PORTS/$NUM_PORTS/g" enb.conf
 sed -i "s/#DL_EARFCN/dl_earfcn = $DL_EARFCN/g" enb.conf
+sed -i "s/LOG_TIME/$TIME/g" enb.conf
 
 srsenb enb.conf
